a0f7f103c6a617e2e1c0d5685fa92a6918352593
JD Solanki <jdsolanki0001@gmail.com>
2022-01-25 19:11:46 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Markdown to HTML
+
+Converts markdown files to HTML
+
+Check action.yml for docs.
+
+Detailed readme coming soon.
---
+++ index.js
@@ -0,0 +1,84 @@
+// Docs: https://docs.github.com/en/actions
+const core = require('@actions/core')
+const { promises: fs } = require('fs')
+const MarkdownIt = require('./markdown-it.min')
+const stylesheetContent = require('./gh-md-style')
+
+const md = new MarkdownIt()
+
+async function mdToHtml(filePath, debug) {
+  // Read source file
+  const content = await fs.readFile(filePath, 'utf8')
+  if (debug) core.info(`content of ${filePath}: ${content}`)
+
+  // converted to HTML
+  const rendered = md.render(content)
+
+  const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<title>Changelog</title>
+<style>
+${stylesheetContent}
+/* recommended style from docs: https://github.com/sindresorhus/github-markdown-css */
+.markdown-body {
+    box-sizing: border-box;
+    min-width: 200px;
+    max-width: 980px;
+    margin: 0 auto;
+    padding: 45px;
+}
+
+@media (max-width: 767px) {
+    .markdown-body {
+        padding: 15px;
+    }
+}
+</style>
+</head>
+<body class="markdown-body">
+${rendered}
+</body>
+</html>`
+
+  return html
+}
+
+(async () => {
+  try {
+    // 👉 Get config
+    const debug = Boolean(JSON.parse(core.getInput('debug')))
+    if (debug) core.info(`debug: ${debug}`)
+
+    const files = JSON.parse(core.getInput('files'))
+    if (debug) core.info(`files: ${files}`)
+
+    // Check if files is array
+    const isFilesOptionValid = Array.isArray(files)
+          // Check if every element of files is array
+          && files.every(ele => Array.isArray(ele)
+          // Check if every element of files have length of 2.
+          && ele.length === 2
+          // Check if every (2) element of files element array is string
+          && ele.every(nestedEle => typeof nestedEle === 'string'))
+
+    if (debug) core.info(`isFilesOptionValid: ${isFilesOptionValid}`)
+
+    if (!isFilesOptionValid) return core.setFailed('files option value is not valid')
+
+    for (let index = 0; index < files.length; index++) {
+      if (debug) core.info(`Working on: ${JSON.stringify(files[index])}`)
+      if (debug) core.info(`writing ${files[index][1]}`)
+
+      if (debug) core.info(`HTML content to write: ${await mdToHtml(files[index][0], debug)}`)
+      await fs.writeFile(files[index][1], await mdToHtml(files[index][0], debug), err => { if (err) core.warning(err) })
+      if (debug) core.info('Written to HTML file')
+    }
+
+    return null
+  } catch (error) {
+    core.setFailed(error.message)
+    return null
+  }
+})()
